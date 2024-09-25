9b866992c62ad33ead51a1af3eb31ba612f27aeb
giridhar512 <32331299+giridhar512@users.noreply.github.com>
2024-09-25 15:12:05 +0530

    Update

--- src/test/java/com/example/products/ProductsPactTest.java
+++ src/test/java/com/example/products/ProductsPactTest.java
@@ -1,5 +1,6 @@
 package com.example.products;
 
+import au.com.dius.pact.core.model.PactSpecVersion;
 import org.junit.jupiter.api.extension.ExtendWith;
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.core.model.RequestResponsePact;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -27,10 +29,9 @@ public class ProductsPactTest {
     PactDslJsonBody body = new PactDslJsonBody();
     body.stringType("name", "product name");
     body.stringType("type", "product series");
-    body.stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850");
+    body.stringType("id", "10");
 
       return builder
-        .given("a product with ID 10 exists")
         .uponReceiving("a request to get a product")
           .path("/product/10")
           .method("GET")
@@ -45,23 +46,22 @@ public class ProductsPactTest {
   public void testGetProduct(MockServer mockServer) throws IOException {
     Product product = new ProductClient().setUrl(mockServer.getUrl()).getProduct("10");
 
-    assertThat(product.getId(), is("5cc989d0-d800-434c-b4bb-b1268499e850"));
+    assertThat(product.getId(), is("10"));
   }
 
   @Pact(consumer="pactflow-example-consumer-java-junit")
   public RequestResponsePact getProducts(PactDslWithProvider builder) {
       return builder
-          .given("a product with ID 10 exists")
           .uponReceiving("a request to get all products")
               .path("/products")
               .method("GET")
           .willRespondWith()
               .status(200)
-              .body(PactDslJsonArray.arrayEachLike()
-                .stringType("name", "product name")
-                .stringType("type", "product series")
-                .stringType("id", "5cc989d0-d800-434c-b4bb-b1268499e850")
-                .closeObject())
+              .body(Objects.requireNonNull(PactDslJsonArray.arrayEachLike()
+                      .stringType("name", "product name")
+                      .stringType("type", "product series")
+                      .stringType("id", "10")
+                      .closeObject()))
           .toPact();
   }
 
@@ -70,6 +70,6 @@ public class ProductsPactTest {
   public void testGetProducts(MockServer mockServer) throws IOException {
     List<Product> products = new ProductClient().setUrl(mockServer.getUrl()).getProducts();
 
-    assertThat(products.get(0).getId(), is("5cc989d0-d800-434c-b4bb-b1268499e850"));
+    assertThat(products.get(0).getId(), is("10"));
   }
 }
